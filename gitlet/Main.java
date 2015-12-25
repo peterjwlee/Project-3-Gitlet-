0868433ed8be286556b9ae1746f69e9f7ea7c98d
peterjwlee <peterjwlee@berkeley.edu>
2015-12-24 17:47:06 -0800

    Update Main.java
    changed

--- gitlet/Main.java
+++ gitlet/Main.java
@@ -14,7 +14,7 @@ import java.nio.file.Files;
 
 
 /** Driver class for Gitlet, the tiny stupid version-control system.
- *  @author Wesley wan and Peter Lee
+ *  @author Peter Lee
  */
 
 public class Main implements Serializable {
